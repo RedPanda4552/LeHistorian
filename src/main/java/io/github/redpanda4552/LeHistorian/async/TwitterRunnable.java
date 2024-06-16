706d54db24cc840fa4cc7c6aeb0faa6f8cfa2b93
RedPanda4552 <redpanda4552@gmail.com>
2024-06-16 02:17:31 -0400

    Show original poster

--- src/main/java/io/github/redpanda4552/LeHistorian/async/TwitterRunnable.java
+++ src/main/java/io/github/redpanda4552/LeHistorian/async/TwitterRunnable.java
@@ -11,7 +11,6 @@ import net.dv8tion.jda.api.interactions.components.buttons.Button;
 public class TwitterRunnable implements Runnable {
 
     private static final Pattern X_PATTERN = Pattern.compile("(\\|\\||)(https:\\/\\/x\\.com\\/.+\\/status\\/[^\\s?]+)[^\\|]*(\\|\\||)");
-    private static final Pattern TWITTER_PATTERN = Pattern.compile("(\\|\\||)(https:\\/\\/twitter\\.com\\/.+\\/status\\/[^\\s?]+)[^\\|]*(\\|\\||)");
 
     private String channelId;
     private String messageId;
@@ -28,26 +27,14 @@ public class TwitterRunnable implements Runnable {
         if (channel != null) {
             Message msg = channel.retrieveMessageById(messageId).complete();
             String contentRaw = msg.getContentRaw();
-            String contentFixed = null;
-
             Matcher xMatcher = X_PATTERN.matcher(contentRaw);
+            StringBuilder sb = new StringBuilder("Tweet originally posted by: ").append(msg.getAuthor().getAsMention()).append("\n");
             
             if (xMatcher.find()) {
                 if (xMatcher.group(1).isEmpty()) {
-                    contentFixed = xMatcher.group(2).replace("https://x.com", "https://fxtwitter.com");
-                }
-            }
-
-            Matcher twitterMatcher = TWITTER_PATTERN.matcher(contentRaw);
-
-            if (twitterMatcher.find()) {
-                if (twitterMatcher.group(1).isEmpty()) {
-                    contentFixed = twitterMatcher.group(2).replace("https://twitter.com", "https://fxtwitter.com");
-                }
-            }
-
-            if (contentFixed != null) {
-                channel.sendMessage(contentFixed)
+                    sb.append(xMatcher.group(2).replace("https://x.com", "https://fxtwitter.com"));
+                    
+                    channel.sendMessage(sb.toString())
                         .setMessageReference(msg.getId())
                         .mentionRepliedUser(false)
                         .addActionRow(
@@ -56,6 +43,7 @@ public class TwitterRunnable implements Runnable {
                             Button.primary("delete-self", "Delete This Message")
                         )
                         .queue();
+                }
             }
         }
     }
